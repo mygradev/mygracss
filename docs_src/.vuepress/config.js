1540b49fec27a6e58f54a8e4aac598e2e79cec8f
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2018-11-24 11:07:04 +0700

    Setup for documentation

---
+++ docs_src/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: 'https://mygradev.github.io/mygracss/',
+  dest: '/home/hammurabi/Documents/Development/Project/CSS/mygracss/docs/',
+  themeConfig: {
+    navbar: false,
+    /*nav: [
+      { text: 'Home', link: '/' },
+    ],
+    sidebar: [
+      '/',
+    ],*/
+  },
+};
---
+++ docs_src/README.md
@@ -0,0 +1,4 @@
+---
+title: MyGraCSS
+---
+# Sedang Dalam Tahap Pengembangan
